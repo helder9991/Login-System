2e89d248b1d92cd5aba11774a1698cbaa20a005a
Helder Afonso <helderafonsoss@hotmail.com>
2021-03-14 15:30:45 -0300

    UserController: Validaçao da requisiçao POST

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,11 +1,31 @@
 import { Request, Response } from 'express';
+import * as Yup from 'yup';
+
+import AppError from '../errors/AppError';
 
 class UserController {
     index(req: Request, res: Response): void {
         res.json({ message: 'index' });
     }
 
-    store(req: Request, res: Response): void {
+    async store(req: Request, res: Response): Promise<void> {
+        const schema = Yup.object().shape({
+            nome: Yup.string().required(),
+            email: Yup.string().email().required(),
+            senha: Yup.string().required(),
+            telefones: Yup.array()
+                .of(
+                    Yup.object().shape({
+                        numero: Yup.string().required(),
+                        ddd: Yup.string().required(),
+                    }),
+                )
+                .required(),
+        });
+
+        if (!(await schema.isValid(req.body)))
+            throw new AppError('Erro na validação da requisição.', 400);
+
         res.json({ message: 'store' });
     }
 }
