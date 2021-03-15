7268b2dc643760a2ce4484bc9517286b6869714a
Helder Afonso <helderafonsoss@hotmail.com>
2021-03-14 23:42:41 -0300

    adiçao da rota auth

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 
+import AuthenticationController from '../controllers/AuthenticationController';
 import UserController from '../controllers/UserController';
 
 const router = Router();
 
 router.get('/user', UserController.index);
 router.post('/user', UserController.store);
+
+router.post('/auth', AuthenticationController.store);
 
 export default router;
