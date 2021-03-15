f7c87876ebae140ce1205353e3e9dc5dd4dc1eab
Helder Afonso <helderafonsoss@hotmail.com>
2021-03-14 22:25:07 -0300

    CreateUser: Criaçao do service que cria o usuario

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,52 @@
+import { getRepository } from 'typeorm';
+
+import User from '../models/User';
+import AppError from '../errors/AppError';
+
+interface IProps {
+    nome: string;
+    email: string;
+    senha: string;
+    telefones: [
+        {
+            numero: string;
+            ddd: string;
+        },
+    ];
+    token: string;
+}
+
+class CreateUserService {
+    async execute({
+        nome,
+        email,
+        senha,
+        telefones,
+        token,
+    }: IProps): Promise<User> {
+        const userRepository = getRepository(User);
+
+        const userExists = await userRepository.findOne({
+            where: { email },
+        });
+
+        if (userExists) throw new AppError('E-mail já existente.', 400);
+
+        const newUser = userRepository.create({
+            nome,
+            email,
+            senha,
+            telefones,
+            ultimo_login: new Date(),
+            data_atualizacao: new Date(),
+            data_criacao: new Date(),
+            token,
+        });
+
+        await userRepository.save(newUser);
+
+        return newUser;
+    }
+}
+
+export default new CreateUserService();
